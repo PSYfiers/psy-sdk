a31362867dfd0702aa2018a782706a89d30ce122
PSYfiers AG <armin.neische@psyfiers.ch>
2023-11-03 14:28:15 +0100

    Change version number

--- test/global.js
+++ test/global.js
@@ -1,6 +1,6 @@
 const Connection = require("../modules/Connection")
+let connectionProduction = new Connection(Connection.SERVER_SSK_PRODUCTION)
+let connectionStaging = new Connection(Connection.SERVER_PSYFIERS_STAGING)
 
-module.exports = {
-    connectionSSK : new Connection(Connection.SERVER_SSK_PRODUCTION),
-    connectionStaging: new Connection(Connection.SERVER_PSYFIERS_STAGING)
-}
+exports.connectionProduction = connectionProduction
+exports.connectionStaging = connectionStaging
--- test/test.js
+++ test/test.js
@@ -1,20 +1,12 @@
-const {connectionStaging} = require("./global")
+const { connectionStaging, connectionProduction } = require("./global")
+
 const Connection = require("../modules/Connection")
 
-describe("Test TAN test",()=>{
-    it("Test", async()=>{
-        let tan = "m7z8wn"        
-        const TanDB = connectionStaging.instance(Connection.TAN)
-        let student = await TanDB.login(tan)
-        
-        console.log(student.toJson())
-        // undefined because of creating the student from a static method.
-        console.log(student.connection)
+async function test() {
+    const TanDB = connectionStaging.newInstance(Connection.TAN)
+    let student = await TanDB.login("m7z8wn")
 
-        const tanDB =connectionStaging.newInstance(Connection.TAN)
-        student = await tanDB.login(tan)
-        console.log(student.toJson())
-        console.log(student.connection.server)
+    console.log(student)
+}
 
-    })
-})
+test()
