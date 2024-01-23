0538e2acb6551bb69a24bd3fb3151263ac762b35
PSYfiers AG <armin.neische@psyfiers.ch>
2024-01-23 11:55:09 +0100

    Resetting sdk to default values

--- test/defaults.js
+++ test/defaults.js
@@ -1,7 +1,6 @@
 module.exports = {
     protocol: "https",
-    hostname: "staging.psyfiers.ch",
-    port: 9765,
+    hostname: "staging.psyfiers.ch",    
     showErrors: true,
     debug: true
 }
